06b35ee16dd0c9aeeed367b46349e459ddcd521c
cvevera <cvevera@gmail.com>
2019-12-11 19:09:05 -0500

    file set up

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devored)
+VALUES ("The Ad Burger", true);
+
+INSERT INTO burgers (burger_name, devored)
+VALUES ("Vegan McWhopper", true);
+
+INSERT INTO burgers (burger_name, devored)
+VALUES ("My Last Burger", true);
