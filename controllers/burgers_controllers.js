f056f74d0f5986b1a93d94d30c4a8ba497ee7aa1
cvevera <cvevera@gmail.com>
2019-12-15 21:56:44 -0500

    created server and linked index.handlebars

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -2,12 +2,16 @@ var express = require("express");
 
 var router = express.Router();
 
+var burgers = require("../models/burger.js");
+
 router.get("/", function(req, res) {
-    burgers.all(function(data) {
-      var hbsObject = {
-        burgers: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
+    // burgers.all(function(data) {
+    //   var hbsObject = {
+    //     burgers: data
+    //   };
+      // console.log(hbsObject);
+      res.render("index");
     });
-  });
+  // });
+
+  module.exports = router;
