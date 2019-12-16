0427adc3fa74cfb9fdcd34a6b9ca40372b97c3fc
cvevera <cvevera@gmail.com>
2019-12-16 04:15:50 -0500

    Rebuilt app using catsMVC code as template

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,11 @@
-INSERT INTO burgers (burger_name, devored)
-VALUES ("The Ad Burger", true);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Ad Burger", false);
 
-INSERT INTO burgers (burger_name, devored)
-VALUES ("Vegan McWhopper", true);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Vegan McWhopper", false);
 
-INSERT INTO burgers (burger_name, devored)
-VALUES ("My Last Burger", true);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("My Last Burger", false);
 
-INSERT INTO burgers (burger_name, devored)
-VALUES ("Baby Burger", true);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Baby Burger", false);
